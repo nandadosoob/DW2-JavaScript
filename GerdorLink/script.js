79d13610c5898f218b0d13b4178dc633730da2d4
nandaBe <130059223+nandadosoob@users.noreply.github.com>
2024-05-03 09:35:48 -0300

    numer

---
+++ GerdorLink/script.js
@@ -0,0 +1,17 @@
+const numeroTel = document.querySelector("numTel");
+
+function mascara(mascaraInput){
+    let valorInput = document.getElementById(`${mascaraInput}Input`)
+    const mascaras{
+
+
+    }
+}
+
+//verificação da quantidade de dígitos
+if (numeroTel.length === 11){
+
+} else{
+}
+
+//\(\d{2}\)\s\d{5}\-\d{4}
