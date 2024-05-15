0617c7380d6a3a80c604a840d010bfdb805b0240
nandadosoob <fer.r.benetti@gmail.com>
2024-05-15 13:31:59 -0300

    caixa p copiar link ok

--- GerdorLink/script.js
+++ GerdorLink/script.js
@@ -11,13 +11,19 @@ numeroTel.addEventListener("input", (evento) => {
     evento.target.value = evento.target.value.replace(/\D\s*/g, "");
     evento.target.value = evento.target.value.replace( /(\d{2})(\d)/, "($1) $2");
     evento.target.value = evento.target.value.replace( /(\d{5})(\d)/, "$1-$2");
-    evento.target.value.style = "green"; 
+    if (numeroTel.value.length === 15){
+        numeroTel.style.color = "green";   
+    } else{
+        numeroTel.style.color = "";
+    }
 });
 
 
 botaoCopiar.addEventListener("click", () => {
+    // copy.style.display = flex;
+    copy.innerHTML = `https://wa.me/55${numeroTel.value.replace(/\D\s*/g, "")}`
     // copy.innerHTML = `https://wa.me/55${numeroTel.value.replace(/\D\s*/g, "")}`;
-    copy.innerHTML = `https://wa.me/55${num}`;
+    // copy.innerHTML = `https://wa.me/55${evento.target.value.replace(/\D\s*/g, "")}`;
     // textoClicar.innerHTML = "Clique no link para copiar";
     // const copiarLink = document.getElementById("link");
     // const cliqueTexto = document.getElementById("clickText");
@@ -30,56 +36,4 @@ abreLink.addEventListener("click", () => {
     aBotao.href = `https://wa.me/55${numeroTel.value.replace(/\D\s*/g, "")}`;
     aBotao.target = "_blank"
 }) 
-
-
-//ATE AQ FUNCIONA
-// const abreLink = document.getElementById("abreOLinkDesktop");
-// abreLink.addEventListener("click", () => {
-//     // const linkDoBotao = document.getElementById("link").innerText;
-//     const aBotao = document.getElementById("botaoAbreLink");
-//     // aBotao.href = linkDoBotao;
-//     aBotao.href = "https://www.w3schools.com/js/js_loop_for.asp"; //da certo
-// })
-
-//funciona
-// const abreLink = document.getElementById("abreOLinkDesktop");
-//     abreLink.addEventListener("click", () => {
-//         const linkDoBotao = `https://wa.me/55${numeroTel.value.replace(/\D\s*/g, "")}`;
-//         const aBotao = document.getElementById("botaoAbreLink");
-//         aBotao.href = linkDoBotao;
-//     )}
-
-
-
-
-// const botaoCopiar = document.getElementById("geraOLinkDesktop");
-// botaoCopiar.addEventListener("click", () => {
-//     var box1 = document.getElementById("geraLink");
-//     const copy = document.createElement("div");
-//     const textoClicar = document.createElement("p");
-//     copy.id = "link";
-//     textoClicar.id = "clickText";
-    
-//     copy.innerHTML = `https://wa.me/55${numeroTel.value.replace(/\D\s*/g, "")}`;
-//     textoClicar.innerHTML = "Clique no link para copiar";
-//     box1.appendChild(copy);
-//     box1.appendChild(textoClicar);
-    
-    
-//     const copiarLink = document.getElementById("link");
-//     const cliqueTexto = document.getElementById("clickText");
-//     copiarLink.addEventListener("click", () => {
-//         navigator.clipboard.writeText(copiarLink.innerHTML).then(() => cliqueTexto.innerHTML = ("Link copiado para a área de transferência."))
-//     })
-
-// })
-
-
-
-//FUNCIONA
-// function geraLink(){
-
-
-
-
 
