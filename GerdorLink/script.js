236fe1d3211de0754fd02e04a1ecedc0b7fe5781
nandadosoob <fer.r.benetti@gmail.com>
2024-05-15 17:19:32 -0300

    so falta o alinhamento dos icones

--- GerdorLink/script.js
+++ GerdorLink/script.js
@@ -5,31 +5,25 @@ const aBotao = document.getElementById("botaoAbreLink");
 const textoClicar = document.getElementById("clickText");
 const copy = document.getElementById("link");
 
-// verificação da quantidade de dígitos
-// const botaoGeraLink = document.getElementById("geraOLinkDesktop");
 numeroTel.addEventListener("input", (evento) => {
     evento.target.value = evento.target.value.replace(/\D\s*/g, "");
     evento.target.value = evento.target.value.replace( /(\d{2})(\d)/, "($1) $2");
     evento.target.value = evento.target.value.replace( /(\d{5})(\d)/, "$1-$2");
     if (numeroTel.value.length === 15){
-        numeroTel.style.color = "green";   
+        numeroTel.style.color = "green";
+        botaoCopiar.style.pointerEvents = "auto";
+        abreLink.style.pointerEvents = "auto";
     } else{
         numeroTel.style.color = "";
     }
 });
 
-
 botaoCopiar.addEventListener("click", () => {
-    // copy.style.display = flex;
+    copy.style.display = "flex"
+    textoClicar.style.display = "flex"
     copy.innerHTML = `https://wa.me/55${numeroTel.value.replace(/\D\s*/g, "")}`
-    // copy.innerHTML = `https://wa.me/55${numeroTel.value.replace(/\D\s*/g, "")}`;
-    // copy.innerHTML = `https://wa.me/55${evento.target.value.replace(/\D\s*/g, "")}`;
-    // textoClicar.innerHTML = "Clique no link para copiar";
-    // const copiarLink = document.getElementById("link");
-    // const cliqueTexto = document.getElementById("clickText");
     copy.addEventListener("click", () => {
     navigator.clipboard.writeText(copy.innerHTML).then(() => textoClicar.innerHTML = ("Link copiado para a área de transferência."))})
-
 })
 
 abreLink.addEventListener("click", () => {
